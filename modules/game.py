974fa4df58068f89ea7d91af2c65c1a0ceb73e1a
h4sohail <hamza.sohail@carleton.ca>
2019-11-04 11:12:40 -0500

    added docstrings

--- modules/game.py
+++ modules/game.py
@@ -12,12 +12,30 @@ class Game():
         self.__state = True
 
     def close(self):  # method to close the game
+        '''
+        Sets the __state attribute to False
+        '''
         self.__state = False
 
     def get_state(self):  # get the current state of the game
+        '''
+        Returns the __state attribute
+        '''
         return self.__state
 
     def save_game(self, player_data, ai_data, game_data): # method to save the player, ai and game data
+        '''
+        Saves all the game info into JSON files under 'saves' sub-directory
+
+        Args:
+            player_data: The player data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            ai_data: The AI data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            game_data: The game data saved as a dictionary {"sessions": int}
+        Returns:
+            boolean: Returns True if the function executes, else returns False
+        Raises:
+            FileExistsError: Raises an exception if a save folder with the same name already exists
+        '''
         while(True):
             selection = input('Would you like to save the game? (y/n): ') # get the choice
             if selection not in ['y', 'n']: # validate the input
@@ -36,12 +54,23 @@ class Game():
                 with open(f'saves/{filename}/ai_data.json', 'w+') as fp:
                     json.dump(ai_data, fp) # save ai data in json format
                 with open(f'saves/{filename}/game_data.json', 'w+') as fp:
-                    json.dump(game_data, fp) # save game data in json format
-                break
+                    json.dump(game_data, fp) # save game data in json format    
+                return True
             else:
-                break
+                return False
             
     def load_game(self): # method to load previous game data
+        '''
+        Loads all the game info from a folder under 'saves' sub-directory
+            
+        Returns:
+            player_data: The player data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            ai_data: The AI data saved as a dictionary {"name": str, "wins": int, "win_rate": []}
+            game_data: The game data saved as a dictionary {"sessions": int}
+            boolean: Returns True if the function executes, else returns False
+        Raises:
+            FileNotFoundError: Raises an exception if a file is not found
+        '''
         while(True):
             selection = input('Would you like to load the game? (y/n): ') # get the choice
             if selection not in ['y', 'n']: # validate the input
@@ -56,11 +85,11 @@ class Game():
                         ai_data = json.load(fp) # read the ai data as json
                     with open(f'saves/{filename}/game_data.json', 'r') as fp:
                         game_data = json.load(fp) # read the game data as json
-                except FileExistsError as identifier:
+                except FileNotFoundError as identifier:
                     print(identifier)
                     continue
                 
-                return (player_data, ai_data, game_data)
+                return (player_data, ai_data, game_data, True)
             else:
-                break
+                return (None, None, None, False)
 
